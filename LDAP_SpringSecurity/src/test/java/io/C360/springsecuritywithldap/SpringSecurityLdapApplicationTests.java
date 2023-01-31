97400f7fd12415a0b549fbc14431dbf27ebcf1c9
InaQureshi <94785605+InaQureshi@users.noreply.github.com>
2023-01-31 16:08:21 +0530

    Soap LDAP Project

---
+++ LDAP_SpringSecurity/src/test/java/io/C360/springsecuritywithldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.C360.springsecuritywithldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
